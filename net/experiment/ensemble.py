209261f0a6b4fd92156e3261d80cad79f919a5b9
劉侑群 <eugene123tw@gmail.com>
2017-09-23 11:07:28 +0100

    add ensemble v2

--- net/experiment/ensemble.py
+++ net/experiment/ensemble.py
@@ -107,6 +107,51 @@ def ensemble_csv_v2(csv_list, out_dir):
     df.to_csv(gz_file, index=False, compression='gzip')
 
 
+
+def ensemble_csv_v3(csv_list, out_dir, chunk_size=1000.0):
+
+    gz_file = out_dir + "/results-ensemble.csv.gz"
+
+    rles = []
+    names = []
+
+    num = 100064
+    H, W = CARVANA_HEIGHT, CARVANA_WIDTH
+    chunk_batch = int(math.ceil(num/chunk_size)) # 100064/1000. = 100.064
+
+    begin, end = 0, 0
+
+    for chunk in range(0, chunk_batch):
+        begin = end
+        end = begin + chunk_size
+        if end > num : end = num
+
+        binary_pred = np.memmap(out_dir + '/preds.npy', dtype=np.uint8, mode='w+', shape=(end-begin, H, W))
+        for file_indices in range(0, len(csv_list)):
+            df = pd.read_csv(csv_list[file_indices], compression='gzip', header=0)
+            for n in range(begin, end):
+                rle = df.values[n][1]
+                binary_pred[n] = binary_pred[n] + (run_length_decode(rle, H=CARVANA_HEIGHT, W=CARVANA_WIDTH) / 255)
+                if file_indices == 0:
+                    names.append(df.values[n][0])
+                if n % 1000 == 0:
+                    print('File indices: %02d/%06d, rle : b/num_test = %06d/%06d' %
+                          (file_indices+1, len(csv_list), n + 1, num)
+                          )
+
+        binary_pred = binary_pred > (len(csv_list) / 2)
+        print("Do run_length_encode")
+        for n in range(begin, end):
+            mask = binary_pred[n]
+            rle = run_length_encode(mask)
+            rles.append(rle)
+            print('rle : b/num_test = %06d/%06d' % (n + 1, num))
+
+    df = pd.DataFrame({'img': names, 'rle_mask': rles})
+    df.to_csv(gz_file, index=False, compression='gzip')
+
+
+
 if __name__ == "__main__":
     csv_list = [
         "/Users/Eugene/Documents/Git/fold1/results-final.csv.gz",
